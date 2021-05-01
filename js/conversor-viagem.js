f621a3246240b28cedf0c4a577626cf581d727eb
Laisses <71999249+Laisses@users.noreply.github.com>
2021-05-01 12:18:11 -0300

    Refatora a função principal em funções separadas, cada uma com a sua responsabilidade.

--- js/conversor-viagem.js
+++ js/conversor-viagem.js
@@ -1,28 +1,37 @@
-function calcularViagem() {
+function obtemDadosViagem() {
+	var escolhas = {
+		dias: parseInt(document.querySelector("#dias").value),
+		passagem: parseFloat(document.querySelector("input[name=passagem]:checked").value),
+		hospedagem: parseFloat(document.querySelector("input[name=hospedagem]:checked").value),
+		alimentacao: parseFloat(document.querySelector("input[name=alimentacao]:checked").value),
+		transporte: parseFloat(document.querySelector("input[name=transporte]:checked").value),
+		passeios: contaPasseios()
+	}
+	return escolhas;
+}
 
-	var cotacaoNZ = 4.0;
-
-	var dias = parseInt(document.querySelector("#dias").value);
-	var passagem = parseFloat(document.querySelector("input[name=passagem]:checked").value);
-	var hospedagem = parseFloat(document.querySelector("input[name=hospedagem]:checked").value);
-	var alimentacao = parseFloat(document.querySelector("input[name=alimentacao]:checked").value);
-	var transporte = parseFloat(document.querySelector("input[name=transporte]:checked").value);
-
+function contaPasseios () {
 	var passeios = 0;
 	var passeioInputs = document.querySelectorAll("input[name=passeio]:checked");
 	for (var i = 0; i < passeioInputs.length; i++) {
 		passeios += parseFloat(passeioInputs[i].value);
 	}
+	return passeios;
+}
 
-	var totalNZ = passagem + hospedagem * dias + alimentacao * dias + transporte * dias + passeios;
-	var totalBR = totalNZ * cotacaoNZ;
+function calculaValorNovaZelandia() {
 
+	var escolhas = obtemDadosViagem();
+	var COTACAO = 3.9;
+
+	var totalNzd = escolhas.passagem + (escolhas.hospedagem * escolhas.dias) + (escolhas.alimentacao * escolhas.dias) + (escolhas.transporte * escolhas.dias) + escolhas.passeios; 
+	var totalBr = totalNzd * COTACAO;
 
 
 	document.querySelector("#n-dias").innerHTML = dias;
-	document.querySelector("#moeda-nz").innerHTML = "NZ$ " + totalNZ.toFixed(2);
-	document.querySelector("#moeda-br").innerHTML = "R$ " + totalBR.toFixed(2);
+	document.querySelector("#moeda-nz").innerHTML = "NZ$ " + totalNzd.toFixed(2);
+	document.querySelector("#moeda-br").innerHTML = "R$ " + totalBr.toFixed(2);
 	document.querySelector("#resultado").classList.remove("esconder");
 }
 
-document.querySelector("#btnCalcular").onclick = calcularViagem;
+document.querySelector("#btnCalcular").onclick = calculaValorNovaZelandia;
