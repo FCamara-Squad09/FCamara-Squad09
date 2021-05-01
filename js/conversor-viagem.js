a32060725e4c3bb85fb0e84b007c932940da8d84
Laisses <71999249+Laisses@users.noreply.github.com>
2021-05-01 13:31:16 -0300

    Adiciona lógica para escrita do valor na tela com a moeda correspondente do país.

--- js/conversor-viagem.js
+++ js/conversor-viagem.js
@@ -41,7 +41,16 @@ function escreveNaTela() {
 
 	document.querySelector("#n-dias").textContent = valor.numeroDeDias;
 
-	document.querySelector("#moeda-local").textContent = "NZ$ " + valor.naMoeda.toFixed(2);
+	if (var COTACAO = 4.42) {
+		document.querySelector("#moeda-local").textContent = "CAD " + valor.naMoeda.toFixed(2);
+	} else if (var COTACAO = 0.05) {
+		document.querySelector("#moeda-local").textContent = "JPY " + valor.naMoeda.toFixed(2);
+	} else if (var COTACAO = 3.90) {
+		document.querySelector("#moeda-local").textContent = "NZD " + valor.naMoeda.toFixed(2);
+	} else {
+		document.querySelector("#moeda-local").textContent = "EUR " + valor.naMoeda.toFixed(2);
+	}
+	
 	document.querySelector("#moeda-br").textContent = "R$ " + valor.emReal.toFixed(2);
 	document.querySelector("#resultado").classList.remove("esconder");
 }
