aca292f4f8c90e7cf0096e7fc1d051eca4046073
Laisses <71999249+Laisses@users.noreply.github.com>
2021-05-05 20:03:08 -0300

    Adiciona função botão voltar ao topo.

--- js/conversor-viagem.js
+++ js/conversor-viagem.js
@@ -1,5 +1,5 @@
 function obtemDadosViagem() {
-	var escolhas = {
+	let escolhas = {
 		dias: parseInt(document.querySelector("#dias").value),
 		passagem: parseFloat(document.querySelector("input[name=passagem]:checked").value),
 		hospedagem: parseFloat(document.querySelector("input[name=hospedagem]:checked").value),
@@ -11,9 +11,9 @@ function obtemDadosViagem() {
 }
 
 function contaPasseios () {
-	var passeios = 0;
-	var passeioInputs = document.querySelectorAll("input[name=passeio]:checked");
-	for (var i = 0; i < passeioInputs.length; i++) {
+	let passeios = 0;
+	let passeioInputs = document.querySelectorAll("input[name=passeio]:checked");
+	for (let i = 0; i < passeioInputs.length; i++) {
 		passeios += parseFloat(passeioInputs[i].value);
 	}
 	return passeios;
@@ -21,12 +21,12 @@ function contaPasseios () {
 
 function calculaValor() {
 
-	var escolhas = obtemDadosViagem();	
+	let escolhas = obtemDadosViagem();	
 
-	var totalViagem = escolhas.passagem + (escolhas.hospedagem * escolhas.dias) + (escolhas.alimentacao * escolhas.dias) + (escolhas.transporte * escolhas.dias) + escolhas.passeios; 
-	var totalBr = totalViagem * COTACAO.moeda;
+	let totalViagem = escolhas.passagem + (escolhas.hospedagem * escolhas.dias) + (escolhas.alimentacao * escolhas.dias) + (escolhas.transporte * escolhas.dias) + escolhas.passeios; 
+	let totalBr = totalViagem * COTACAO.moeda;
 
-	var valorFinal = {
+	let valorFinal = {
 		naMoeda: totalViagem,
 		emReal: totalBr,
 		numeroDeDias: escolhas.dias,
@@ -38,7 +38,7 @@ function calculaValor() {
 
 function escreveNaTela() {
 
-	var valor = calculaValor();
+	let valor = calculaValor();
 
 	document.querySelector("#n-dias").textContent = valor.numeroDeDias;
 
@@ -46,19 +46,13 @@ function escreveNaTela() {
 	
 	document.querySelector("#moeda-br").textContent = "R$ " + valor.emReal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2});
 	document.querySelector("#resultado").classList.remove("esconder");
+	let body = document.body.clientHeight;
+	window.scrollTo(0, body);
 }
 
-document.querySelector("#btnCalcular").onclick = escreveNaTela;
-
-
-/*
 function voltarTopoTela(){
-	var voltarTopo = document.querySelector(".voltarTopo");
-
-	voltarTopo.addEventListener("click", function(){
-		window.scrollTo(0,0);
-	});
+	window.scrollTo(0,0);	
 }
 
-document.querySelector(".voltarTopo").onclick = voltarTopoTela;
-*/
+document.querySelector(".btn-topo").onclick = voltarTopoTela;
+document.querySelector("#btnCalcular").onclick = escreveNaTela;
