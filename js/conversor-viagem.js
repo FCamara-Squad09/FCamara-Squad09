29ac225c893a04836f50b2df8b1550150df7876f
Karina Ramos <81200958+karinnaramos@users.noreply.github.com>
2021-05-06 16:28:01 -0300

    Revert "Padronizando lay"

--- js/conversor-viagem.js
+++ js/conversor-viagem.js
@@ -1,5 +1,5 @@
 function obtemDadosViagem() {
-	let escolhas = {
+	var escolhas = {
 		dias: parseInt(document.querySelector("#dias").value),
 		passagem: parseFloat(document.querySelector("input[name=passagem]:checked").value),
 		hospedagem: parseFloat(document.querySelector("input[name=hospedagem]:checked").value),
@@ -11,9 +11,9 @@ function obtemDadosViagem() {
 }
 
 function contaPasseios () {
-	let passeios = 0;
-	let passeioInputs = document.querySelectorAll("input[name=passeio]:checked");
-	for (let i = 0; i < passeioInputs.length; i++) {
+	var passeios = 0;
+	var passeioInputs = document.querySelectorAll("input[name=passeio]:checked");
+	for (var i = 0; i < passeioInputs.length; i++) {
 		passeios += parseFloat(passeioInputs[i].value);
 	}
 	return passeios;
@@ -21,12 +21,12 @@ function contaPasseios () {
 
 function calculaValor() {
 
-	let escolhas = obtemDadosViagem();	
+	var escolhas = obtemDadosViagem();	
 
-	let totalViagem = escolhas.passagem + (escolhas.hospedagem * escolhas.dias) + (escolhas.alimentacao * escolhas.dias) + (escolhas.transporte * escolhas.dias) + escolhas.passeios; 
-	let totalBr = totalViagem * COTACAO.moeda;
+	var totalViagem = escolhas.passagem + (escolhas.hospedagem * escolhas.dias) + (escolhas.alimentacao * escolhas.dias) + (escolhas.transporte * escolhas.dias) + escolhas.passeios; 
+	var totalBr = totalViagem * COTACAO.moeda;
 
-	let valorFinal = {
+	var valorFinal = {
 		naMoeda: totalViagem,
 		emReal: totalBr,
 		numeroDeDias: escolhas.dias,
@@ -38,21 +38,27 @@ function calculaValor() {
 
 function escreveNaTela() {
 
-	let valor = calculaValor();
+	var valor = calculaValor();
 
 	document.querySelector("#n-dias").textContent = valor.numeroDeDias;
 
-	document.querySelector("#moeda-local").textContent = valor.cambio + " " + valor.naMoeda.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2});
+	document.querySelector("#moeda-local").textContent = valor.cambio + valor.naMoeda.toFixed(2);
 	
-	document.querySelector("#moeda-br").textContent = "R$ " + valor.emReal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2});
+	document.querySelector("#moeda-br").textContent = "R$ " + valor.emReal.toFixed(2);
 	document.querySelector("#resultado").classList.remove("esconder");
-	let body = document.body.clientHeight;
-	window.scrollTo(0, body);
 }
 
-function voltarTopoTela(){
-	window.scrollTo(0,0);	
-}
-
-document.querySelector(".btn-topo").onclick = voltarTopoTela;
 document.querySelector("#btnCalcular").onclick = escreveNaTela;
+
+
+/*
+function voltarTopoTela(){
+	var voltarTopo = document.querySelector(".voltarTopo");
+
+	voltarTopo.addEventListener("click", function(){
+		window.scrollTo(0,0);
+	});
+}
+
+document.querySelector(".voltarTopo").onclick = voltarTopoTela;
+*/
